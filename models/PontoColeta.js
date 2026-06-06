e91f3e59a86f78a9515cf2724be7734cae01b746
Luiz Gustavo <157995536+LuizGustavo139@users.noreply.github.com>
2026-06-05 22:36:48 -0300

    define model de Residuo com atributos nome e descricao

--- models/PontoColeta.js
+++ models/PontoColeta.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../../config/database');
+const sequelize = require('../config/database');
 
 const PontoColeta = sequelize.define('PontoColeta', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
