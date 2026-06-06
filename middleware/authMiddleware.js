a9c7837cf54fc0d7d6f9ea6fe04fc474820a3fe5
Luiz Gustavo <157995536+LuizGustavo139@users.noreply.github.com>
2026-06-05 22:34:33 -0300

    fix: resolve conflito no authMiddleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -11,11 +11,7 @@ module.exports = async (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-<<<<<<< HEAD
     
-=======
-    
->>>>>>> 380b667e78c393617802902ac8bb37d7139e124a
     const user = await User.findByPk(decoded.id, { attributes: ['id', 'nome', 'email'] });
     if (!user) {
       res.clearCookie('token');
@@ -23,11 +19,7 @@ module.exports = async (req, res, next) => {
     }
 
     req.user = user;
-<<<<<<< HEAD
-    res.locals.user = user; 
-=======
     res.locals.user = user;
->>>>>>> 380b667e78c393617802902ac8bb37d7139e124a
     next();
   } catch (error) {
     res.clearCookie('token');
