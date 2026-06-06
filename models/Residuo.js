aa808bcca35ded399f55a297b08e47af8f895833
Luiz Gustavo <157995536+LuizGustavo139@users.noreply.github.com>
2026-06-05 22:37:15 -0300

    define model de Residuo com atributos nome e descricao

--- models/Residuo.js
+++ models/Residuo.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../../config/database');
+const sequelize = require('../config/database');
 
 const Residuo = sequelize.define('Residuo', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
