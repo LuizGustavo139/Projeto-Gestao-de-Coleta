fb2a43d0299bd4866f00f2c7d6881ba487df193a
Marcos-edu <marcosseduardos2005@gmail.com>
2026-06-06 02:12:40 -0300

    security(routes): aplica middleware de autenticacao e adiciona rota de delecao

--- routes/pontoRoutes.js
+++ routes/pontoRoutes.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const pontoController = require('../controllers/pontoController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+// Protege todas as rotas abaixo com o middleware de autenticação
+router.use(authMiddleware);
 
 router.get('/', pontoController.getAll);
 router.get('/novo', pontoController.renderCreate);
 router.post('/novo', pontoController.create);
-
-// Rotas para renderização e atualização da edição (Adicionadas agora)
 router.get('/editar/:id', pontoController.renderEdit);
 router.post('/editar/:id', pontoController.update);
+
+// Rota para deletar agendamento (Adicionada agora)
+router.get('/deletar/:id', pontoController.delete);
 
 module.exports = router;
