768f5172e03f93c5a5c7ed19ae3dd40ab84416e4
Marcos-edu <marcosseduardos2005@gmail.com>
2026-06-06 02:09:41 -0300

    feat(routes): adiciona rota GET para renderizar formulario de novo agendamento

--- routes/pontoRoutes.js
+++ routes/pontoRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const pontoController = require('../controllers/pontoController');
 
-// Rota principal para o Dashboard
 router.get('/', pontoController.getAll);
+
+// Rota para renderizar o formulário de novo agendamento (Adicionada agora)
+router.get('/novo', pontoController.renderCreate);
 
 module.exports = router;
