d2bc62adf5d16859c9e806a03b6ec358eaeb604c
Marcos-edu <marcosseduardos2005@gmail.com>
2026-06-06 02:12:14 -0300

    feat(routes): adiciona rotas GET e POST para edicao de agendamentos

--- routes/pontoRoutes.js
+++ routes/pontoRoutes.js
@@ -4,8 +4,10 @@ const pontoController = require('../controllers/pontoController');
 
 router.get('/', pontoController.getAll);
 router.get('/novo', pontoController.renderCreate);
-
-// Rota POST para processar e criar o agendamento (Adicionada agora)
 router.post('/novo', pontoController.create);
+
+// Rotas para renderização e atualização da edição (Adicionadas agora)
+router.get('/editar/:id', pontoController.renderEdit);
+router.post('/editar/:id', pontoController.update);
 
 module.exports = router;
