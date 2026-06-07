a7080f811b12d29dbc2f2ff9ffd806ab1c3b63a5
Marcos-edu <marcosseduardos2005@gmail.com>
2026-06-07 10:38:12 -0300

    Update pontoRoutes.js

--- routes/pontoRoutes.js
+++ routes/pontoRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const pontoController = require('../controllers/pontoController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-// Protege todas as rotas abaixo com o middleware de autenticação
 router.use(authMiddleware);
 
 router.get('/', pontoController.getAll);
@@ -12,7 +11,6 @@ router.post('/novo', pontoController.create);
 router.get('/editar/:id', pontoController.renderEdit);
 router.post('/editar/:id', pontoController.update);
 
-// Rota para deletar agendamento (Adicionada agora)
 router.get('/deletar/:id', pontoController.delete);
 
 module.exports = router;
