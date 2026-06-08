e5836dc9298e24bcaeac18305ad293a4c53c34cb
LuizGustavo139 <luizgustavog939@gmail.com>
2026-06-08 20:27:11 -0300

    Inserção de um admin

--- models/User.js
+++ models/User.js
@@ -2,10 +2,28 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const User = sequelize.define('User', {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  nome: { type: DataTypes.STRING, allowNull: false },
-  email: { type: DataTypes.STRING, allowNull: false, unique: true },
-  senha: { type: DataTypes.STRING, allowNull: false }
+    id: { 
+        type: DataTypes.INTEGER, 
+        primaryKey: true, 
+        autoIncrement: true 
+    },
+    nome: { 
+        type: DataTypes.STRING, 
+        allowNull: false 
+    },
+    email: { 
+        type: DataTypes.STRING, 
+        allowNull: false, 
+        unique: true 
+    },
+    senha: { 
+        type: DataTypes.STRING, 
+        allowNull: false 
+    },
+    isAdmin: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    }
 });
 
-module.exports = User;
+module.exports = module.exports = User;
